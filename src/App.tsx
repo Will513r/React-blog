09b97cf22683619de00ea64991ca1ebd0b220c91
William <will513r@gmail.com>
2024-06-28 20:41:09 -0400

    debugged tailwinds

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <img src='/underdog.jpg'></img>
+        <img src="/underdog.jpg"></img>
       </div>
-      <h1>Frontend Blog</h1>
+      <h1 className="text-7xl">Frontend Blog</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>If your bored click this
-         the count is {count}
+        <button onClick={() => setCount((count) => count + 1)}>
+          If your bored click this the count is {count}
         </button>
-        
       </div>
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
