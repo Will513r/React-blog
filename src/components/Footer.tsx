dc091657a5d5e0b76505d4348fe78500a1327e00
Christopher Forti <christopher.forti.79@gmail.com>
2024-07-08 22:11:05 -0400

    Added react logo to footer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,5 @@
 import Udlogo from "../assets/underdog.jpg";
+import Reactlogo from "../assets/react.svg";
 
 export function Footer() {
   return (
@@ -6,6 +7,10 @@ export function Footer() {
       <img
         src={Udlogo}
         className="absolute bottom-0 left-4 top-0 m-auto h-10 w-auto"
+      />
+      <img
+        src={Reactlogo}
+        className="absolute bottom-0 right-4 top-0 m-auto h-10 w-auto"
       />
       <hr className="justify-center" />
       <div className="m-5">
