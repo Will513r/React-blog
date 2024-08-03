c0bc0501d84853776a9fa54a7548628edd343949
Christopher Forti <christopher.forti.79@gmail.com>
2024-08-03 19:45:43 -0400

    Used a fat arrow to work in headerNavigation error.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ type HeaderProps = {
 };
 
 export function Header({ headerNavigation }: HeaderProps) {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   function navigationToggle() {
     setIsOpen(!isOpen);
@@ -16,7 +16,9 @@ export function Header({ headerNavigation }: HeaderProps) {
       <div className="container mx-auto flex items-center justify-between px-10 py-6">
         <div>
           <a
-            onClick={navigationToggle}
+            onClick={() => {
+              headerNavigation;
+            }}
             href="/Mainpage"
             className="bg-gradient-to-tr from-indigo-600 to-green-600 bg-clip-text text-4xl font-bold text-transparent hover:cursor-pointer"
           >
@@ -26,14 +28,12 @@ export function Header({ headerNavigation }: HeaderProps) {
         <nav className="flex items-center">
           <ul className="hidden items-center space-x-4 sm:flex">
             <li>
-              <a href="/Mainpage">
-                <button
-                  onClick={navigationToggle}
-                  className="text-md from-indigo-600 to-green-600 hover:text-indigo-600"
-                >
-                  Home
-                </button>
-              </a>
+              <button
+                onClick={navigationToggle}
+                className="text-md from-indigo-600 to-green-600 hover:text-indigo-600"
+              >
+                <a href="/Mainpage">Home</a>
+              </button>
             </li>
           </ul>
           <div className="ml-8 hidden items-center space-x-4 md:flex lg:ml-12"></div>
